7bd0f321d470efce94873588c4b415d970848a1a
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-05-13 10:48:32 -0400

    initial commit

---
+++ comp123-lesson1B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+*Author: dhruti patel
+*Date:may 13,2016
+*date modified: may13,2016
+*student id: 300857555
+*description: this program demonstartes github and version control
+*best practices.
+*version :0.1
+*/
+
+
+
+
+
+namespace comp123_lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
